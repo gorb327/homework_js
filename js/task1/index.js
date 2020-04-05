384b54554c25d3218ebaee2530bd76c1be68f5db
gorb327 <58131648+gorb327@users.noreply.github.com>
2020-04-05 11:03:57 +0300

    Add files via upload

---
+++ js/task1/index.js
@@ -0,0 +1,6 @@
+const nomber_day = prompt ("какое сегодня число?")
+
+if (nomber_day < 2) {
+    console.log ("Сегодня первое число")
+}
+
---
+++ js/task5/index.js
@@ -0,0 +1,8 @@
+const eagle = 1;
+const tails = 2;
+
+Math.random(eagle || tails);
+    if (eagle) {
+        alert("ФАРТ")
+    }
+
